2890a5e0ac0f88482d83d23111a15f58c69bcf52
Thaynna Carolliny <tatacarolliny24@gmail.com>
2023-04-25 11:49:47 -0300

    form

---
+++ script.js
@@ -0,0 +1,22 @@
+function valida() {
+    var email = document.getElementById("email").value;
+    var nome = document.getElementById("nome").value;
+    var data_de_nascimento = document.getElementById("Data").value;
+    var cpf = document.getElementById("CPF").value;
+    var cidade = document.getElementById("cidade").value;
+    var rua = document.getElementById("rua").value;
+    var numero = document.getElementById("numero").value;
+    var senha = document.getElementById("Senha").value;
+    var repetir_Senha = document.getElementById("repetir").value;
+    // var tell = document.getElementById('tell').value; -> Bug para pegar o valor
+
+    if (email&&nome&&data_de_nascimento&&cpf&&cidade&&rua&&numero&senha&&repetir_Senha) {
+        if (senha === repetir_Senha) {
+            alert('Cadastro realizado com sucesso!');
+        } else {
+            alert('As senha não estão iguais!');
+        };
+    } else {
+        alert('Preencha todos os campos!');
+    };
+};
