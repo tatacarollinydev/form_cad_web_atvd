7271e0093aba3d670b7ad220315fe0787477d5b3
Thaynna Carolliny <tatacarolliny24@gmail.com>
2023-04-26 12:25:25 -0300

    bug

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function valida() {
     var email = document.getElementById("email").value;
     var nome = document.getElementById("nome").value;
     var data_de_nascimento = document.getElementById("Data").value;
+    var tell = document.getElementById("tell").value;
     var cpf = document.getElementById("CPF").value;
     var cidade = document.getElementById("cidade").value;
     var rua = document.getElementById("rua").value;
@@ -10,7 +11,7 @@ function valida() {
     var repetir_Senha = document.getElementById("repetir").value;
     // var tell = document.getElementById('tell').value; -> Bug para pegar o valor
 
-    if (email&&nome&&data_de_nascimento&&cpf&&cidade&&rua&&numero&senha&&repetir_Senha) {
+    if (email&&nome&&data_de_nascimento&&cpf&&cidade&&rua&&numero&senha&&repetir_Senha&&tell) {
         if (senha === repetir_Senha) {
             alert('Cadastro realizado com sucesso!');
         } else {
